25b4a79b87bb965455c668b7eccd132febfe302b
soferpel <so.fernandez@opendeusto.es>
2024-02-23 09:00:38 +0100

    create namespace

---
+++ Assets/1/HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SampleNameSpace { 
+    public class HelloWorld : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log("Hello World with namespace 1");
+        }
+    }
+}
